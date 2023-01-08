17537c97b4a4224ff98092d589012969279af432
WILLINTONG REY GOMEZ <willintong.rey3528@correo.policia.gov.co>
2023-01-08 10:11:33 -0500

    finish crud country

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+        }
+    }
+}
---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        //data anotations
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+
+    }
+}
